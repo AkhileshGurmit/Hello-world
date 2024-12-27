4ebecd0c15fc8c0c0c774193b281ccd3bd50b379
Akhilesh <95014375+AkhileshGurmit@users.noreply.github.com>
2024-12-27 17:45:50 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,66 @@
+console.log("Hello World from Sharad Jayappa");
+
+// let firstName = prompt("Enter your first name");
+// let middleName = prompt("Enter your middle name");
+// let lastName = prompt("Enter Your last name ");
+// let years = prompt("enter your age")
+
+// console.log(` ${firstName} ${middleName} ${lastName} is ${years} old`);
+// console.error(`${firstName} ${middleName} ${lastName} is ${years} old`);
+// console.warn(`${firstName} ${middleName} ${lastName} is ${years} old`);
+
+// alert(`Welcome Topper All Rounder ${firstName} ${middleName} ${lastName} is ${years} old`);
+
+// let num=20;
+
+// if(num%10==0){
+//     console.log("good");
+// } else{
+//     console.log("bad");
+// }
+
+// let quarter=3;
+
+// switch(quarter){
+//     case 1:
+//         console.log("jfm");
+//         break;
+//     case 2:
+//         console.log("amj");
+//         break;    
+//     case 3:
+//         console.log("jja");
+//         break;
+//     case 4:
+//         console.log("son");
+//         break;
+//     default:
+//         console.log("none");
+// }
+
+// let a=9;
+// let b=5;
+// let c=3;
+
+// if(a>b&&a>c){
+//     console.log("a is greatest that is" + a);
+// }else if(b>a&&b>c){
+//     console.log("b is greatest that is" + b);
+// }else{
+//     console.log("c is greatest that is" + c);
+
+// }
+
+let a =32;
+let b=47852;
+
+if(a%10==b%10){
+    console.log("same last digit i.e "+ a%10);
+} else{
+    console.log("not same");
+}
+
+
+
+
+
